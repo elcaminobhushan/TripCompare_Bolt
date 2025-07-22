e2969e1af78e90cea85a3382536226eaf6fafcfb
Bhushan Bhosale <53351455+elcaminobhushan@users.noreply.github.com>
2025-07-23 00:34:54 +0530

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config'; // important for CLI usage
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase credentials. Please set VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY in your .env file.");
